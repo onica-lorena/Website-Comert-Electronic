93afcf672a740c4a7cef6ee5de4e484d593da0c4
Onica Lorena-Andreea <onica.lorena05@gmail.com>
2025-01-07 11:43:08 +0200

    Update search.php

--- src/search.php
+++ src/search.php
@@ -8,7 +8,7 @@ include 'connection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rezultatele Căutării</title>
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="styles/style2.css">
 </head>
 
 <body>
